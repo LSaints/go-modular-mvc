c3ab283a2c1c1430fd49252223b6655f34cb224f
Mateus Leandro <lsants.dev@gmail.com>
2024-10-30 17:22:22 -0300

    chore: carregar modulo de usuario

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,32 +2,26 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
 
-	userRoutes "github.com/LSaints/go-modular-mvc/internal/core/user-management/routes"
+	user_management "github.com/LSaints/go-modular-mvc/internal/core/user-management"
 	server_vars "github.com/LSaints/go-modular-mvc/internal/shared/config/env_vars/load_server_vars"
 	"github.com/LSaints/go-modular-mvc/internal/shared/database"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	_, err := database.Load()
+	err := server_vars.Execute()
+	_, err = database.Load()
 
-	err = server_vars.Execute()
 	if err != nil {
 		panic(err)
 	}
 
 	r := gin.Default()
-
-	currentDir, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
-	r.LoadHTMLGlob(filepath.Join(currentDir, "internal", "core", "user-management", "views", "templates") + "/*")
-
-	userRoutes.InitRoutes(r)
+	loadModules(r)
 	r.Run(fmt.Sprintf(":%s", server_vars.SERVER_PORT))
+}
+
+func loadModules(r *gin.Engine) {
+	user_management.Load(r)
 }
