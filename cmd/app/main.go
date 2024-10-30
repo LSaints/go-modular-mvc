84b6a2d58ae8d8f29e57d55186f5acb76a214f5a
Mateus Leandro <lsants.dev@gmail.com>
2024-10-30 16:07:37 -0300

    feat: carregar modulos criados

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
 	userRoutes "github.com/LSaints/go-modular-mvc/internal/core/user-management/routes"
+	server_vars "github.com/LSaints/go-modular-mvc/internal/shared/config/env_vars/load_server_vars"
+	"github.com/LSaints/go-modular-mvc/internal/shared/database"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	_, err := database.Load()
+
+	err = server_vars.Execute()
+	if err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 
 	currentDir, err := os.Getwd()
@@ -19,5 +29,5 @@ func main() {
 	r.LoadHTMLGlob(filepath.Join(currentDir, "internal", "core", "user-management", "views", "templates") + "/*")
 
 	userRoutes.InitRoutes(r)
-	r.Run(":8080")
+	r.Run(fmt.Sprintf(":%s", server_vars.SERVER_PORT))
 }
