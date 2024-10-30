7887a273513f2a1df682534f68af6dda40b94f20
Mateus Leandro <lsants.dev@gmail.com>
2024-10-30 17:22:54 -0300

    delete: remover items temporarios

--- internal/core/user-management/user.go
+++
@@ -1,8 +0,0 @@
-package user_management
-
-type User struct {
-	ID       string `json:"id" gorm:"primary_key"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"-"`
-}
