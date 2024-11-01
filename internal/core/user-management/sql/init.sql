ee924f636c20477798ae8e29b353d49fb45356cc
Mateus Leandro <lsants.dev@gmail.com>
2024-11-01 13:59:15 -0300

    feat: adicionar arquivo sql para criar tabela do modulo de user-management

---
+++ internal/core/user-management/sql/init.sql
@@ -0,0 +1,8 @@
+USE db_modular
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
