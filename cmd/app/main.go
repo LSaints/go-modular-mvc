24816808845fbc7fd3c591b792a3e5d89f8d2cd8
Mateus Leandro <lsants.dev@gmail.com>
2024-11-03 21:21:26 -0300

    chore: adicionar loadViews dos modulos no main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path"
+	"path/filepath"
 
+	booksmanagement "github.com/LSaints/go-modular-mvc/internal/core/books-management"
 	usermanagement "github.com/LSaints/go-modular-mvc/internal/core/user-management"
 	loadsrvvars "github.com/LSaints/go-modular-mvc/internal/shared/config/env_vars/load_server_vars"
 	"github.com/LSaints/go-modular-mvc/internal/shared/database"
 	"github.com/LSaints/go-modular-mvc/internal/shared/http/adapters"
 	"github.com/LSaints/go-modular-mvc/internal/shared/http/interfaces"
 )
+
+func Init(r interfaces.Router) {
+	booksmanagement.Load(r)
+	usermanagement.Load(r)
+	loadViews(r)
+}
 
 func main() {
 	err := loadsrvvars.Execute()
@@ -18,17 +28,53 @@ func main() {
 		panic(err)
 	}
 
+	currentDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	staticFilesPath := path.Join(currentDir, "internal", "shared", "web", "static")
+
 	router := adapters.NewGinRouter()
+	Init(router)
+	router.Static("/static", staticFilesPath)
+
 	if router == nil {
 		panic("Falha ao criar GinRouter")
 	}
-	loadModules(router)
+
 	err = router.Run(fmt.Sprintf(":%s", loadsrvvars.SERVER_PORT))
 	if err != nil {
 		panic(err)
 	}
 }
 
-func loadModules(r interfaces.Router) {
-	usermanagement.Load(r)
+func loadViews(r interfaces.Router) error {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+
+	// module: user-management
+
+	// feature: find all
+	UserfindAllPage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "find_all", "web", "views", "list.html")
+
+	// feature: create
+	UsercreatePage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "create", "web", "views", "register.html")
+	UsersuccessPage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "create", "web", "views", "success.html")
+	UsererrorPage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "create", "web", "views", "error.html")
+
+	// feature: update
+	UserupdatePage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "update", "web", "views", "update.html")
+	UsererrorUpdatePage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "update", "web", "views", "error.html")
+	UsersuccessUpdatePage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "update", "web", "views", "success.html")
+
+	// module: books-management
+
+	// feature: find all
+	BooksfindAllPage := filepath.Join(currentDir, "internal", "core", "books-management", "features", "find_all", "web", "views", "listBooks.html")
+
+	r.LoadHTMLFiles(UserfindAllPage, UsercreatePage, UsersuccessPage, UsererrorPage, UserupdatePage, UsererrorUpdatePage, UsersuccessUpdatePage, BooksfindAllPage)
+
+	return nil
 }
--- internal/core/user-management/module.go
+++ internal/core/user-management/module.go
@@ -1,9 +1,6 @@
 package usermanagement
 
 import (
-	"os"
-	"path/filepath"
-
 	createRoute "github.com/LSaints/go-modular-mvc/internal/core/user-management/features/create/http"
 	deleteUser "github.com/LSaints/go-modular-mvc/internal/core/user-management/features/delete/http"
 	findAllRoute "github.com/LSaints/go-modular-mvc/internal/core/user-management/features/find_all/http"
@@ -13,7 +10,6 @@ import (
 )
 
 func Load(r interfaces.Router) {
-	loadViews(r)
 	registerRoutes(r)
 }
 
@@ -24,28 +20,4 @@ func registerRoutes(r interfaces.Router) {
 	deleteUser.LoadRoutes(r)
 	update.LoadRoutes(r)
 
-}
-
-func loadViews(r interfaces.Router) error {
-	currentDir, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-
-	// feature: find all
-	findAllPage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "find_all", "web", "views", "list.html")
-
-	// feature: create
-	createPage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "create", "web", "views", "register.html")
-	successPage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "create", "web", "views", "success.html")
-	errorPage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "create", "web", "views", "error.html")
-
-	// feature: update
-	updatePage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "update", "web", "views", "update.html")
-	errorUpdatePage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "update", "web", "views", "error.html")
-	successUpdatePage := filepath.Join(currentDir, "internal", "core", "user-management", "features", "update", "web", "views", "success.html")
-
-	r.LoadHTMLFiles(findAllPage, createPage, successPage, errorPage, updatePage, errorUpdatePage, successUpdatePage)
-
-	return nil
 }
